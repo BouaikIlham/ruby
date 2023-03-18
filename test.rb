3ae176c8f6ac8a4e0e43f14dbc8c74d4f712a0ea
ilham bouaik <ibouaik@gmail.com>
2023-03-18 19:30:28 +0000

    stock_picker

--- test.rb
+++ test.rb
@@ -21,19 +21,58 @@
 
 
 
-def substrings(text, dictionary)
-    # create an empty hash
-    result = Hash.new(0)
-    lowered_text = text.downcase
-    # iterate over every word in dictionary
-    dictionary.each do |word| 
-        matches = lowered_text.scan(word).length
-        result[word] = matches unless matches == 0
-    end 
+# def substrings(text, dictionary)
+#     dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+#     # create an empty hash
+#     result = Hash.new(0)
+#     lowered_text = text.downcase
+#     # iterate over every word in dictionary
+#     dictionary.each do |word| 
+#         matches = lowered_text.scan(word).length
+#         result[word] = matches unless matches == 0
+#     end 
     
-    puts result
+#     puts result
+# end
+
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+#  puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+dictionary = ["my","name","is","going","horn","how","ilham","it","i","low","bouaik","movies","partner","sports"]
+
+# p 'Enter you text'
+# text = gets.chomp
+# result = Hash.new
+# dictionary.each do |word|
+#     matches = text.scan(word).length
+#     result[word] =  matches unless matches == 0
+# end
+# p result
+
+# dictionary = %w[below down go going horn how howdy it i low own part partner sit i am gonna create a large dictionary so that it will be helpful]
+# p 'enter text:'
+# search = gets.chomp
+# filtered_search = search.downcase
+# words = filtered_search.split(' ')
+# p words
+# result = Hash.new
+# words.each do |word|
+#   result[word] += 1 if dictionary.include?(word)
+# end
+# p result
+
+# sould return the best day of sell and buy
+def stock_picker(prices)
+    res = []
+    prices.each_with_index do |price, i|
+       highest_val = prices[i..].max   
+       highest_idx = prices[i..].each_with_index.max[1] + i
+       res.push([highest_val - price,i, highest_idx])
+    end
+     res.max_by(&:first)
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts stock_picker([17,3,6,9,15,8,6,1,10])
 
- puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
